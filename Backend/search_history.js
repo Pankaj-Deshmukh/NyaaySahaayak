43c089f0c09265f73e79a9a58d557f9764080f96
Chashmmish <mahathireddyj9815@gmail.com>
2024-01-02 11:05:44 +0530

    i changed name of mongoose file

--- Backend/search_history.js
+++ Backend/search_history.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { connectToDatabase, mongoose } = require('./mongo2');
+const { connectToDatabase, mongoose } = require('./mongo_search_history');
 
 const app = express();
 const PORT = 3000;
