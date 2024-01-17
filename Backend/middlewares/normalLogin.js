23a37223397f14f93ecf7ed890a7ffeeacf307ee
kalyanppc <kalyanppc@gmail.com>
2024-01-17 11:22:00 +0530

    started authentication

---
+++ Backend/middlewares/normalLogin.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../passwords");
+
+function normalLoginMiddleware(req,res,next){
+      
+}
+module.exports = normalLoginMiddleware;
