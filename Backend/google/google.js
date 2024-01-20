eee2359355caefb8fb004d08a70e1dcc0655abe1
Chashmmish <mahathireddyj9815@gmail.com>
2024-01-20 11:38:52 +0530

    changed

--- Backend/google/google.js
+++ Backend/google/google.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const passport = require('passport');
+const GoogleStrategy=require('passport-google-oauth2').Strategy;
 const session = require('express-session');
 const { UserModel } = require('../db/index');
 const cors = require('cors');
 require('./google_passport');
-// const http = require('http');
-const app = express();
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentials: true,
-// };
 
-// app.use(cors(corsOptions));
+
+const app = express();
+
+
+passport.use(new GoogleStrategy({
+  clientID:"601511902773-425guolbghpg62c970bp268hrv54cjvk.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "http://localhost:4000/auth/callback",
+  passReqToCallback:true
+},function(request,accessToken,refreshToken,profile,done){
+    done(null,profile)
+  }));
+  
+passport.serializeUser((user,done)=>{
+  done(null,user)
+}),
+passport.deserializeUser((user,done)=>{
+ done(null,user)
+}),
+
+
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
